5df4b979bfc71567786cce3f955d4e2b2d8d5720
Yifan Chen <ethanyc216@gmail.com>
2019-12-03 19:09:27 -0800

    upload hw9

--- HW9/WeatherApp/app/src/main/java/com/ethanchen/weatherapp/PicassoClient.java
+++ HW9/WeatherApp/app/src/main/java/com/ethanchen/weatherapp/PicassoClient.java
@@ -8,6 +8,7 @@ import com.squareup.picasso.Picasso;
 public class PicassoClient {
 
     public  static void downloadImage(Context c, String url, ImageView img) {
-        Picasso.with(c).load(url).fit().centerInside().into(img);
+        //Picasso.with(c).load(url).fit().centerInside().into(img);
+        Picasso.with(c).load(url).fit().centerCrop().into(img);
     }
 }
